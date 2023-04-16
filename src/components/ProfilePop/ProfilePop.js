782c4ae682d8f114f1a1d0c7f686fc1d0b55ef80
Zapper <72506463+Zappingmadnnes@users.noreply.github.com>
2023-04-16 17:13:12 +0300

    Profile popup mobile fix

--- src/components/ProfilePop/ProfilePop.js
+++ src/components/ProfilePop/ProfilePop.js
@@ -3,6 +3,7 @@ import Button from "../Button/Button";
 
 import { GrClose } from "react-icons/gr";
 function ProfilePop({ closeFunction }) {
+	const isMobile = window.innerWidth <= 768;
 	function handleClose() {
 		closeFunction(false);
 	}
@@ -15,8 +16,28 @@ function ProfilePop({ closeFunction }) {
 				</div>
 				<h1>Skolēna Profils</h1>
 				<h2>123456-12345</h2>
-				<h3>Pieteiktās laimīgās stundas: 2</h3>
+				<h3>Pieteiktās laimīgās stundas: 3</h3>
 				<div className="ProfilePop-cards">
+					<div className="ProfilePop-card">
+						17. februāris
+						<br />
+						15:30 - 16:55
+						<br />
+						Matemātika ll
+						<br />
+						Integrēšana
+						<div className="ProfilePop-card__button">Atcelt</div>
+					</div>
+					<div className="ProfilePop-card">
+						17. februāris
+						<br />
+						15:30 - 16:55
+						<br />
+						Matemātika ll
+						<br />
+						Integrēšana
+						<div className="ProfilePop-card__button">Atcelt</div>
+					</div>
 					<div className="ProfilePop-card">
 						17. februāris
 						<br />
